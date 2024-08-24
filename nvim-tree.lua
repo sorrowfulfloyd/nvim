7fbc82d832e953290a88dd89987b43190b296570
sorrowfulfloyd <gogercin.nurullah@gmail.com>
2024-08-24 14:49:53 +0300

    Revert "changed to nvimtree to be toggleable"
    This reverts commit ff0b8b1d347731a842c5d5f2b040cbd5ff8c3db7.

---
+++ nvim-tree.lua
@@ -0,0 +1,14 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {}
+  end,
+}
