b16698ac662c36f384889445fb2948bb1fac7e6f
sorrowfulfloyd <gogercin.nurullah@gmail.com>
2024-10-02 17:58:08 +0300

    seperate transparency plugin wooo

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -1,57 +1,45 @@
 return {
 	"nvim-lualine/lualine.nvim",
-	opts = function()
-		-- Use the "auto" theme as the base
-		local lualine_theme = require("lualine.themes.auto")
-
-		-- Make the background of all theme components transparent
-		for _, mode in pairs(lualine_theme) do
-			for _, section in pairs(mode) do
-				section.bg = "none"
-			end
-		end
-
-		return {
-			options = {
-				icons_enabled = true,
-				theme = lualine_theme, -- Apply the modified theme
-				component_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
-				disabled_filetypes = {
-					statusline = {},
-					winbar = {},
-				},
-				ignore_focus = {},
-				always_divide_middle = true,
-				globalstatus = false,
-				refresh = {
-					statusline = 1000,
-					tabline = 1000,
-					winbar = 1000,
-				},
+	opts = {
+		options = {
+			icons_enabled = true,
+			theme = "auto",
+			component_separators = { left = "", right = "" },
+			section_separators = { left = "", right = "" },
+			disabled_filetypes = {
+				statusline = {},
+				winbar = {},
 			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "filename" },
-				lualine_x = { "encoding", "fileformat", "filetype" },
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
+			ignore_focus = {},
+			always_divide_middle = true,
+			globalstatus = false,
+			refresh = {
+				statusline = 1000,
+				tabline = 1000,
+				winbar = 1000,
 			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = { "location" },
-				lualine_y = {},
-				lualine_z = {},
-			},
-			tabline = {},
-			winbar = {},
-			inactive_winbar = {},
-			extensions = {},
-		}
-	end,
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff", "diagnostics" },
+			lualine_c = { "filename" },
+			lualine_x = { "encoding", "fileformat", "filetype" },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = { "filename" },
+			lualine_x = { "location" },
+			lualine_y = {},
+			lualine_z = {},
+		},
+		tabline = {},
+		winbar = {},
+		inactive_winbar = {},
+		extensions = {},
+	},
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -45,17 +45,17 @@ return {
 	lazy = false,
 	priority = 1000,
 	config = function()
-		require("rose-pine").setup({
-			disable_background = true,
-		})
-
+		-- require("rose-pine").setup({
+		-- 	disable_background = true,
+		-- })
+		--
 		vim.cmd.colorscheme("rose-pine")
-
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-		vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
-		vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
-		vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
-		vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
+		--
+		-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
 	end,
 }
