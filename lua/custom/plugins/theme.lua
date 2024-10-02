80e5e4eb86eb6b74d13ee719561afaccee7c1738
sorrowfulfloyd <gogercin.nurullah@gmail.com>
2024-10-02 10:41:07 +0300

    I'm gonna be transparent with you guys AYAYA

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -39,11 +39,23 @@
 -- 	end,
 -- }
 --
+
 return {
 	"rose-pine/neovim",
 	lazy = false,
 	priority = 1000,
 	config = function()
+		require("rose-pine").setup({
+			disable_background = true,
+		})
+
 		vim.cmd.colorscheme("rose-pine")
+
+		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+		vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+		vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
+		vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
 	end,
 }
