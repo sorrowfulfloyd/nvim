4e33505e282e235295d362f6501f3a61ddb7a218
sorrowfulfloyd <gogercin.nurullah@gmail.com>
2025-04-24 02:50:44 +0300

    debloating my nvim setup uwu

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -122,9 +122,5 @@ vim.keymap.set("n", "<leader>sG", ":LiveGrepGitRoot<cr>", { desc = "[S]earch by 
 
 vim.opt.termguicolors = true
 
--- my custom whichkey keymaps
---[[ require('which-key').register {
-  ['<leader>x'] = { name = 'E[x]plorer', _ = 'which_key_ignore' },
-} ]]
 vim.filetype.add({ extension = { templ = "templ" } })
 return {}
